78da2309edbeba851be971db6f662a6a9ab62576
Max <maxts@stud.ntnu.no>
2020-02-04 14:33:54 +0100

    Added øving 1

---
+++ Øving 1/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Max Schau on 31/01/2020.
+//
+
+#include "main.h"
---
+++ Øving 1/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Max Schau on 31/01/2020.
+//
+
+#ifndef ØVING_1_MAIN_H
+#define ØVING_1_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //ØVING_1_MAIN_H
