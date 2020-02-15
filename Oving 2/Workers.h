93a7784efbfc3a84271641f0a69158bc3fb9dcdf
Max <maxts@stud.ntnu.no>
2020-02-15 12:42:53 +0100

    Finished oving 2 hopefully

--- Oving 2/Workers.h
+++ Oving 2/Workers.h
@@ -5,11 +5,90 @@
 #ifndef OVING_2_WORKERS_H
 #define OVING_2_WORKERS_H
 
+#include <thread>
+#include <list>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+
+using namespace std;
 
 class Workers {
+    int amount;
+    vector<thread> threads;
+    mutex task_mutex;
+    condition_variable cv;
+    int counter;
+    list<function<void()>> tasks;
+    mutex done_mutex;
+    bool done = false;
 
+
+public:
+
+
+    Workers(int amountInput) {
+        counter = amountInput;
+    }
+
+
+    void start() {
+
+        for (int i = 0; i < counter; i++) {
+            function<void()> task;
+            threads.emplace_back([this, &task] {
+                while (true) {
+                    function<void()> task;
+                    {
+                        unique_lock<mutex> ul(this->task_mutex);
+                        this->cv.wait(ul, [this] {
+                            return this->done || !this->tasks.empty();
+                        });
+                        if (this->tasks.empty() && this->done)
+                            return;
+                        task = *tasks.begin();
+                        tasks.pop_front();
+                    }
+                    task();
+                }
+            });
+
+        };
+
+
+
+    }
+
+    void stop() {
+        {
+            unique_lock<mutex> uniqueLock(this->done_mutex);
+            done = true;
+        }
+        this->cv.notify_all();
+        for (auto &t : this->threads) t.join();
+        threads.empty();
+    }
+
+
+    void post(function<void()> f) {
+        {
+            unique_lock<mutex> uniqueLock(task_mutex);
+            tasks.emplace_back(f);
+        }
+        cv.notify_one();
+    };
+
+    void post_timeout(function<void()> f, int ms) {
+        this_thread::sleep_for(chrono::milliseconds(ms));
+        {
+            unique_lock<mutex> uniqueLock(task_mutex);
+            tasks.emplace_back(f);
+        }
+        cv.notify_one();
+
+
+    }
 
 };
-
 
 #endif //OVING_2_WORKERS_H
--- Oving 2/main.cpp
+++ Oving 2/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include "Workers.h"
+#include <mutex>
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    mutex task_mutex;
+
+    Workers worker_threads(4);
+    Workers event_loop(1);
+    worker_threads.start();
+    event_loop.start();
+
+    worker_threads.post([] {
+        cout << "WORKERTHREADS TASK 1 running" << endl;
+    });
+
+    worker_threads.post([] {
+        cout << "WORKERTHREADS TASK 2 running" << endl;
+    });
+
+    event_loop.post([] {
+        cout << "EVENTLOOP Task 1 running" << endl;
+    });
+
+    event_loop.post([] {
+        cout << "EVENTLOOP Task 2 running" << endl;
+    });
+
+    event_loop.post_timeout([] {
+        cout << "EVENTLOOP Task 3 running" << endl;
+    }, 2000);
+
+    event_loop.post_timeout([] {
+        cout << "EVENTLOOP Task 4 running" << endl;
+    }, 100);
+
+    worker_threads.stop();
+    event_loop.stop();
+
+
+
+
+
+
+
+
     return 0;
 }
